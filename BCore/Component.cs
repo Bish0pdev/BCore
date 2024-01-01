f7a60b90cdd41a94f13db58bddcb2b0b97e777b0
Bish0pdev <Ytthepienetwork@gmail.com>
2024-01-01 10:38:54 -0600

    Added docstings

--- BCore/Component.cs
+++ BCore/Component.cs
@@ -7,73 +7,168 @@ using System.Runtime.CompilerServices;
 
 namespace BCore
 {
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    /// <summary>
+    /// Represents a base class for components that can be attached to entities.
+    /// </summary>
     public abstract class Component
     {
+        /// <summary>
+        /// The entity to which this component is attached.
+        /// </summary>
         protected Entity entity = new();
 
+        /// <summary>
+        /// Initializes the component with the specified entity and calls the OnInitialize method.
+        /// </summary>
+        /// <param name="entity">The entity to which this component is attached.</param>
         public void Initialize(Entity entity)
         {
             this.entity = entity;
             OnInitialize();
         }
 
-        public virtual void Update(GameTime gameTime) { }
-        public virtual void Draw(SpriteBatch batch) {  }
+        /// <summary>
+        /// Performs initialization logic for the component.
+        /// </summary>
+        protected virtual void OnInitialize() { }
 
-        protected virtual void OnInitialize(){}
+        /// <summary>
+        /// Updates the component's logic based on the game time.
+        /// </summary>
+        /// <param name="gameTime">Snapshot of the game's timing state.</param>
+        public virtual void Update(GameTime gameTime) { }
+
+        /// <summary>
+        /// Draws the component using the provided SpriteBatch.
+        /// </summary>
+        /// <param name="batch">The SpriteBatch used for rendering.</param>
+        public virtual void Draw(SpriteBatch batch) { }
     }
 
+    /// <summary>
+    /// Represents a component that renders a sprite.
+    /// </summary>
     public class SpriteRenderer : Component
     {
+        /// <summary>
+        /// The texture of the sprite.
+        /// </summary>
         public Texture2D texture;
+
+        /// <summary>
+        /// The color tint of the sprite.
+        /// </summary>
         public Color color = Color.White;
 
+        /// <summary>
+        /// Initializes a new instance of the SpriteRenderer class with a texture and color.
+        /// </summary>
+        /// <param name="texture">The texture of the sprite.</param>
+        /// <param name="color">The color tint of the sprite.</param>
         public SpriteRenderer(Texture2D texture, Color color)
         {
             this.texture = texture;
             this.color = color;
         }
+
+        /// <summary>
+        /// Initializes a new instance of the SpriteRenderer class with a texture and default color (White).
+        /// </summary>
+        /// <param name="_texture">The texture of the sprite.</param>
         public SpriteRenderer(Texture2D _texture)
         {
             texture = _texture;
             color = Color.White;
         }
 
+        /// <summary>
+        /// Draws the sprite using the provided SpriteBatch.
+        /// </summary>
+        /// <param name="batch">The SpriteBatch used for rendering.</param>
         public override void Draw(SpriteBatch batch)
         {
             batch.Draw(texture, entity.Position, null, color, entity.Rotation, entity.Origin, entity.Scale, SpriteEffects.None, 0f);
         }
+
+        /// <summary>
+        /// Performs additional initialization logic by setting the origin based on the texture's center.
+        /// </summary>
         protected override void OnInitialize()
         {
             entity.Origin = texture.Bounds.Center.ToVector2();
         }
     }
 
+    /// <summary>
+    /// Represents a component that renders text using a SpriteFont.
+    /// </summary>
     public class TextRenderer : Component
     {
+        /// <summary>
+        /// The font used for rendering text.
+        /// </summary>
         public SpriteFont font;
+
+        /// <summary>
+        /// The color of the rendered text.
+        /// </summary>
         public Color color = Color.White;
+
+        /// <summary>
+        /// The text to be rendered.
+        /// </summary>
         public string Text = "";
 
-        public TextRenderer(SpriteFont font,string text, Color color)
+        /// <summary>
+        /// Initializes a new instance of the TextRenderer class with a font, text, and color.
+        /// </summary>
+        /// <param name="font">The font used for rendering text.</param>
+        /// <param name="text">The text to be rendered.</param>
+        /// <param name="color">The color of the rendered text.</param>
+        public TextRenderer(SpriteFont font, string text, Color color)
         {
             this.font = font;
             this.color = color;
             this.Text = text;
         }
+
+        /// <summary>
+        /// Initializes a new instance of the TextRenderer class with a font and color.
+        /// </summary>
+        /// <param name="font">The font used for rendering text.</param>
+        /// <param name="color">The color of the rendered text.</param>
         public TextRenderer(SpriteFont font, Color color)
         {
             this.font = font;
             this.color = color;
         }
+
+        /// <summary>
+        /// Initializes a new instance of the TextRenderer class with a font.
+        /// </summary>
+        /// <param name="font">The font used for rendering text.</param>
         public TextRenderer(SpriteFont font) { this.font = font; }
+
+        /// <summary>
+        /// Sets the text to be rendered.
+        /// </summary>
+        /// <param name="text">The text to be rendered.</param>
         public void SetText(string text)
         {
             Text = text;
         }
+
+        /// <summary>
+        /// Draws the text using the provided SpriteBatch.
+        /// </summary>
+        /// <param name="batch">The SpriteBatch used for rendering.</param>
         public override void Draw(SpriteBatch batch)
         {
-            batch.DrawString(font,Text,entity.Position,color);
+            batch.DrawString(font, Text, entity.Position, color);
         }
     }
+
 }
--- BCore/Entity.cs
+++ BCore/Entity.cs
@@ -4,18 +4,54 @@ using System.Collections.Generic;
 
 namespace BCore
 {
+    using System.Collections.Generic;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    /// <summary>
+    /// Represents an entity in a game world with position, rotation, scale, and components.
+    /// </summary>
     public class Entity
     {
+        /// <summary>
+        /// Gets or sets the position of the entity.
+        /// </summary>
         public Vector2 Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rotation angle of the entity.
+        /// </summary>
         public float Rotation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the scale of the entity.
+        /// </summary>
         public Vector2 Scale { get; set; }
+
+        /// <summary>
+        /// Gets or sets the origin point of the entity.
+        /// </summary>
         public Vector2 Origin { get; set; } = Vector2.Zero;
 
         private List<Component> components = new List<Component>();
 
-        public Entity() 
-        { Position = Vector2.Zero; Rotation = 0; Scale = Vector2.One; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Entity"/> class with default values.
+        /// </summary>
+        public Entity()
+        {
+            Position = Vector2.Zero;
+            Rotation = 0;
+            Scale = Vector2.One;
+        }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Entity"/> class with specified parameters.
+        /// </summary>
+        /// <param name="position">The initial position of the entity.</param>
+        /// <param name="rotation">The initial rotation angle of the entity.</param>
+        /// <param name="scale">The initial scale of the entity.</param>
+        /// <param name="components">The list of components to be attached to the entity.</param>
         public Entity(Vector2 position, float rotation, Vector2 scale, List<Component> components)
         {
             Position = position;
@@ -23,23 +59,39 @@ namespace BCore
             Scale = scale;
             this.components = components;
         }
-        
 
+        /// <summary>
+        /// Moves the entity to the specified position.
+        /// </summary>
+        /// <param name="position">The new position of the entity.</param>
         public void MoveTo(Vector2 position)
         {
             Position = position;
         }
 
+        /// <summary>
+        /// Rotates the entity by the specified angle.
+        /// </summary>
+        /// <param name="angle">The angle to rotate the entity by.</param>
         public void Rotate(float angle)
         {
             Rotation += angle;
         }
 
+        /// <summary>
+        /// Sets the scale of the entity.
+        /// </summary>
+        /// <param name="scale">The new scale of the entity.</param>
         public void SetScale(Vector2 scale)
         {
             Scale = scale;
         }
 
+        /// <summary>
+        /// Adds a component to the entity and initializes it.
+        /// </summary>
+        /// <param name="component">The component to be added.</param>
+        /// <returns>The added component.</returns>
         public Component AddComponent(Component component)
         {
             components.Add(component);
@@ -47,6 +99,11 @@ namespace BCore
             return component;
         }
 
+        /// <summary>
+        /// Gets the first component of type T attached to the entity.
+        /// </summary>
+        /// <typeparam name="T">The type of the component to retrieve.</typeparam>
+        /// <returns>The component of type T if found; otherwise, null.</returns>
         public T GetComponent<T>() where T : Component
         {
             foreach (var component in components)
@@ -60,7 +117,10 @@ namespace BCore
             return null;
         }
 
-        // Update all components attached to the entity
+        /// <summary>
+        /// Updates all components attached to the entity.
+        /// </summary>
+        /// <param name="gameTime">Snapshot of the game's timing state.</param>
         public virtual void Update(GameTime gameTime)
         {
             foreach (var component in components)
@@ -69,7 +129,10 @@ namespace BCore
             }
         }
 
-        // Draw all components attached to the entity
+        /// <summary>
+        /// Draws all components attached to the entity.
+        /// </summary>
+        /// <param name="batch">The SpriteBatch used for rendering.</param>
         public virtual void Draw(SpriteBatch batch)
         {
             foreach (var component in components)
@@ -78,4 +141,5 @@ namespace BCore
             }
         }
     }
+
 }
